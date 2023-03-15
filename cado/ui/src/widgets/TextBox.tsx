b9d4940abad85467d726f3b036c75b58a814295a
Chris Gregory <gregorybchris@gmail.com>
2023-03-15 14:59:34 -0700

    Change the font

--- cado/ui/src/widgets/TextBox.tsx
+++ cado/ui/src/widgets/TextBox.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
 export default function Button(props: ButtonProps) {
   return (
     <input
-      className="inline-block w-24 rounded-md bg-rock py-2 px-4 text-sm outline-none duration-150 placeholder:text-lighter-rock focus:bg-light-rock active:ease-linear"
+      className="inline-block w-24 rounded-md bg-rock py-2 px-4 text-sm font-bold outline-none duration-150 placeholder:text-lighter-rock focus:bg-light-rock active:ease-linear"
       type="text"
       value={props.value}
       placeholder={props.placeholder}
