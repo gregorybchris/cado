54e74450136ebbb1da4ab339e78421c1babafc11
Chris Gregory <gregorybchris@gmail.com>
2023-03-15 18:28:55 -0700

    Disable React strict mode

--- cado/ui/src/index.tsx
+++ cado/ui/src/index.tsx
@@ -5,11 +5,6 @@ import "@fontsource/poppins/700.css";
 import "./index.css";
 
 import App from "./components/App";
-import React from "react";
 import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
